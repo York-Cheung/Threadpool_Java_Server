990aee093b8e3e58d2ca7cdf41888ebdac4c360b
Yorkson <434998827@qq.com>
2017-04-24 17:21:27 +0800

    init

---
+++ src/RqProcessor.java
@@ -0,0 +1,108 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.URLConnection;
+import java.nio.file.Files;
+import java.util.Date;
+import java.util.StringTokenizer;
+import java.util.logging.Logger;
+
+/**
+ * Created by Yorkson on 2017/4/24.
+ */
+public class RqProcessor implements Runnable{
+    private final static Logger logger = Logger.getLogger(RqProcessor.class.getCanonicalName());
+    private File rootDirectory;
+    private String INDEX_FILE = "Lena.png";
+    private Socket connection;
+
+    public RqProcessor(File rootDirectory, String indexFile, Socket request) {
+        if (rootDirectory.isFile()){
+            throw new IllegalArgumentException("rootDirectory must be a directory, not a file");
+        }
+        try {
+            rootDirectory = rootDirectory.getCanonicalFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.rootDirectory = rootDirectory;
+        this.connection = request;
+        if (INDEX_FILE==null)
+            this.INDEX_FILE = indexFile;
+    }
+    private void sendHeader(Writer out, String responseCode, String contentType, int length) throws IOException {
+        out.write(responseCode+"\r\n");
+        Date now = new Date();
+        out.write("Date:"+now+"\r\n");
+        out.write("Server:JHTTP 1.0\r\n");
+        out.write("Content-length:"+length+"\r\n");
+        out.write("Content-Type:"+contentType+"\r\n");
+        out.write("\r\n");
+        out.flush();
+    }
+    private void sendHeader(Writer out, String responseCode) throws IOException {
+        out.write(responseCode+"\r\n");
+        Date now = new Date();
+        out.write("Date:"+now+"\r\n");
+        out.write("Server:JHTTP 1.0\r\n");
+        out.write("\r\n");
+        out.flush();
+    }
+    @Override
+    public void run(){
+        String root = rootDirectory.getPath();
+        try {
+            OutputStream raw = new BufferedOutputStream(connection.getOutputStream());
+            Writer out = new OutputStreamWriter(raw);
+            Reader in  = new InputStreamReader(new BufferedInputStream((connection.getInputStream())),"ASCII");
+            StringBuffer request = new StringBuffer(80);
+            while(true) {
+                int c = in.read();
+                if (c=='\r'||c=='\n'||c == -1) {
+                    break;
+                }
+                request.append((char)c);
+            }
+            String get = request.toString();
+            logger.info(connection.getRemoteSocketAddress()+" "+get);
+            StringTokenizer st = new StringTokenizer(get);
+            String method=st.nextToken();
+            String version = "";
+
+            if (method.equals("GET")){
+                String fileName = st.nextToken();
+                if (fileName.endsWith("/")) fileName += INDEX_FILE;
+                String contentType = URLConnection.getFileNameMap().getContentTypeFor(fileName);
+                if(st.hasMoreTokens()){
+                    version=st.nextToken();
+                }
+                File theFile = new File(rootDirectory,fileName.substring(1,fileName.length()));
+                System.out.println(theFile);
+                if (!theFile.exists()){
+                    sendHeader(out,"HTTP/1.1 404 Not Found");
+//                    TODO
+//                    theFile = new File(rootDirectory,"errorPage.html");
+//                    byte[] theData = Files.readAllBytes(theFile.toPath());
+//                    raw.write(theData);
+//                    raw.flush();
+                }else if (!theFile.canRead()||!theFile.getCanonicalPath().startsWith(root)){
+                    sendHeader(out,"HTTP/1.1 403 NO READ PERMISSIONS");
+                }
+                else {//检测所请求文件是否超出根目录
+                    byte[] theData = Files.readAllBytes(theFile.toPath());
+                    if (theData == null){
+                        sendHeader(out,"500 INTERNAL SERVER ERROR");
+                    }else if (version.startsWith("HTTP/")){
+                        System.out.printf("OK");
+                        sendHeader(out,"HTTP/1.1 200 OK",contentType,theData.length);
+                    }
+                    raw.write(theData);
+                    raw.flush();
+                }
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/ServerPool.java
@@ -0,0 +1,64 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
+/**
+ * Created by Yorkson on 2017/4/24.
+ */
+public class ServerPool {
+    private static final Logger logger = Logger.getLogger(HttpServer.class.getCanonicalName());
+    private static final int NUM_THREADS = 10;
+    private static final String INDEX_FILE = "Lena.png";
+
+    private final File rootDirectory;
+    private final int port;
+    public ServerPool(File rootDirectory,int port) throws IOException {
+        if (!rootDirectory.isDirectory()){
+            throw new IOException(rootDirectory+" does not exit as a directory");
+        }
+        this.rootDirectory = rootDirectory;
+        this.port = port;
+    }
+
+    private void start(){
+        ExecutorService pool = Executors.newFixedThreadPool(NUM_THREADS);
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            logger.info("Accepting connections on port:"+serverSocket.getLocalPort());
+            logger.info("Document root:"+rootDirectory);
+
+            while (true){
+                try {
+                    Socket request = serverSocket.accept();
+                    Runnable r  = new RqProcessor(rootDirectory,INDEX_FILE,request);
+                    pool.submit(r);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        File docroot;
+        try {
+            docroot = new File("/Users/Yorkson/IdeaProjects/Threadpool_Java_Server/");
+        }catch (ArrayIndexOutOfBoundsException ex){
+            return;
+        }
+        try {
+            ServerPool server = new ServerPool(docroot,8080);
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
